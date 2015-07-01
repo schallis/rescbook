9e4663de306dd8f7b61c820f862c4a2792709b68
Steven Challis <steven.challis@hogarthww.com>
2015-07-01 12:52:06 -0400

    Even simpler server

--- server.js
+++ server.js
@@ -1,38 +1,15 @@
 var express = require('express');
-var http = require('http');
-// var socketio = require('socket.io');
-// var io = socketio(http);
 var app = express();
-var server = http.Server(app);
 
-app.get('/', function(req, res){
-  res.sendfile('./index.html');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
-//
-// var types = [
-//   'Fire',
-//   'Medical',
-//   'Police'
-// ]
-//
-// function sendNewIncident(socket, num) {
-//   socket.emit('newIncident', {
-//     'type': types[Math.floor(Math.random()*types.length)],
-//     'status': 'Pending',
-//     'datetime': new Date().toISOString(),
-//     'num': num
-//   });
-//   console.log('sending new incident...');
-// };
-//
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-//   setTimeout(function() {sendNewIncident(socket, 3)}, 3000);
-//   setTimeout(function() {sendNewIncident(socket, 4)}, 8000);
-//   setTimeout(function() {sendNewIncident(socket, 5)}, 15000);
-// });
 
-var port = process.env.PORT || 3000;
-server.listen(port, function(){
-  console.log('listening on *:' + port);
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
 });
